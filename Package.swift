4d7a66dcc71f2f5ab94bbe49e5cc2582c92ed0ca
ser <122270051+ser-0xff@users.noreply.github.com>
2023-11-17 11:55:03 +0300

    fix(patch): [sc-455] Make DistributedSystemConformance static. (#14)

--- Package.swift
+++ Package.swift
@@ -56,7 +56,6 @@ let package = Package(
         ),
         .library(
             name: "DistributedSystemConformance",
-            type: .dynamic,
             targets: ["DistributedSystemConformance"]
         ),
         .executable(
