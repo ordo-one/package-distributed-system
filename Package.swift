6b73c582b72ff9064a28045f99b8393c61f5c429
ser-0xff <122270051+ser-0xff@users.noreply.github.com>
2023-11-23 18:44:02 +0300

    Cosmetic fix.

--- Package.swift
+++ Package.swift
@@ -13,15 +13,13 @@ let externalDependencies: [String: Range<Version>] = [
 ]
 
 let internalDependencies: [String: Range<Version>] = [
-    // Internal:
     "package-latency-tools": .upToNextMajor(from: "1.0.0"),
-    // Internal, but public:
     "package-benchmark": .upToNextMajor(from: "1.0.0"),
     "package-concurrency-helpers": .upToNextMajor(from: "2.0.0"),
     "package-consul": .upToNextMajor(from: "3.0.0"),
     "package-datetime": .upToNextMajor(from: "1.0.1"),
-    "package-frostflake": .upToNextMajor(from: "3.1.0"),
-    "package-distributed-system-conformance": .upToNextMajor(from: "1.0.0"),
+    "package-frostflake": .upToNextMajor(from: "4.0.0"),
+    "package-distributed-system-conformance": .upToNextMajor(from: "2.0.0"),
 ]
 
 func makeDependencies() -> [Package.Dependency] {
