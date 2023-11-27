5786c89b64d72e526cc3dfda98ee72d66699c063
ordo-ci <104988168+ordo-ci@users.noreply.github.com>
2023-11-27 18:56:12 +0300

    feat(major): bump semver Bump dependency version [sc-5038] (#19)
    * feat(major): semver bump Bump dependency version [sc-5038]
    
    * Fix build failure.
    
    ---------
    
    Co-authored-by: ser-0xff <122270051+ser-0xff@users.noreply.github.com>

--- Sources/DistributedSystem/RemoteCallDecoder.swift
+++ Sources/DistributedSystem/RemoteCallDecoder.swift
@@ -6,24 +6,28 @@ public struct RemoteCallDecoder: DistributedTargetInvocationDecoder {
     public typealias SerializationRequirement = DistributedSystemConformance.Transferable
 
     private var genericSubstitutions: [Any.Type]
-    private var arguments: ByteBuffer
+    private var buffer: ByteBuffer
+    private var arguments: [Deserializable]
 
     init(envelope: InvocationEnvelope) {
         genericSubstitutions = envelope.genericSubstitutions
-        arguments = envelope.arguments
+        buffer = envelope.arguments
+        arguments = []
     }
 
     public mutating func decodeNextArgument<Argument: SerializationRequirement>() throws -> Argument {
-        let size = try arguments.readWithUnsafeReadableBytes { ptr in try ULEB128.decode(ptr, as: UInt.self) }
+        let size = try buffer.readWithUnsafeReadableBytes { ptr in try ULEB128.decode(ptr, as: UInt.self) }
 
-        guard let slice = arguments.readSlice(length: Int(size)) else {
+        guard let slice = buffer.readSlice(length: Int(size)) else {
             throw DistributedSystemErrors.decodeError(description: "Failed to decode argument")
         }
 
         return try slice.withUnsafeReadableBytes { ptr in
             // The 'Argument' type can do nothing but just copy data from the buffer
             // TODO: probably would worth to invent a possibility to retain the buffer for the 'Argument' instance lifetime (sc-1307)
-            try Argument(fromSerializedBuffer: ptr)
+            let argument = try Argument(fromSerializedBuffer: ptr)
+            arguments.append(argument)
+            return argument
         }
     }
 
@@ -39,5 +43,11 @@ public struct RemoteCallDecoder: DistributedTargetInvocationDecoder {
     public mutating func decodeReturnType() throws -> Any.Type? {
         // Return type is not supported in this version
         nil
+    }
+
+    public func _releaseArguments() {
+        for argument in arguments {
+            argument._releaseBuffer()
+        }
     }
 }
