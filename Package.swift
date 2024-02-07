f67cd32f19ddf6f5d7383c42b2cf3d9b618a54ed
ser-0xff <122270051+ser-0xff@users.noreply.github.com>
2024-02-07 16:20:29 +0300

    Bump dependencies

--- Package.swift
+++ Package.swift
@@ -14,10 +14,10 @@ let externalDependencies: [String: Range<Version>] = [
 
 let internalDependencies: [String: Range<Version>] = [
     "package-benchmark": .upToNextMajor(from: "1.0.0"),
-    "package-concurrency-helpers": .upToNextMajor(from: "2.0.0"),
+    "package-concurrency-helpers": .upToNextMajor(from: "4.0.0"),
     "package-consul": .upToNextMajor(from: "3.0.0"),
     "package-datetime": .upToNextMajor(from: "1.0.1"),
-    "package-distributed-system-conformance": .upToNextMajor(from: "3.0.0"),
+    "package-distributed-system-conformance": .upToNextMajor(from: "4.0.0"),
     "package-frostflake-xcf": .upToNextMajor(from: "1.0.12"),
     "package-latency-tools": .upToNextMajor(from: "1.0.0"),
     "package-unsafe-retained-xcf": .upToNextMajor(from: "1.0.3"),
