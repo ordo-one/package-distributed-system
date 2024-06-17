37a23ab8a79e0413f23ea301170c78d20468de69
ser-0xff <122270051+ser-0xff@users.noreply.github.com>
2024-06-17 11:54:29 +0300

    Cosmetic fix.

--- Sources/DistributedSystem/DiscoveryManager.swift
+++ Sources/DistributedSystem/DiscoveryManager.swift
@@ -1,4 +1,4 @@
-// Copyright 2023 Ordo One AB
+// Copyright 2024 Ordo One AB
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
