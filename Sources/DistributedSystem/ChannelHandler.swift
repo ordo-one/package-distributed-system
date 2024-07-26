f651214e35d72f6f2e5de30de6139e7b0383e130
ser-0xff <122270051+ser-0xff@users.noreply.github.com>
2024-07-26 12:11:34 +0300

    Cosmetic fix.

--- Sources/DistributedSystem/ChannelHandler.swift
+++ Sources/DistributedSystem/ChannelHandler.swift
@@ -61,7 +61,7 @@ class ChannelHandler: ChannelInboundHandler {
     func channelInactive(context: ChannelHandlerContext) {
         // 2024-03-07T12:48:24.806241+02:00 DEBUG ds : [DistributedSystem] [IPv4]192.168.0.9/192.168.0.9:53019/1: channel inactive ["port": 55056]
         // TODO: it would be nice to know "name/type" of remote process
-        logger.info("\(context.channel.addressDescription)/\(Self.self): connection closed (ChannelHandler)")
+        logger.info("\(context.channel.addressDescription)/\(Self.self): connection closed")
         actorSystem.channelInactive(id, address)
     }
 
